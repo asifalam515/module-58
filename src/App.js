9713c9e2c2a2f828e26396c52cc75fd5cebe2fb1
asifalam515 <asibulalam515@gmail.com>
2021-12-29 15:07:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Home from './component/Home/Home';
+import Login from './component/Home/Login/Login';
+import Register from './component/Register/Register';
+import Header from './component/Header/Header';
+import AuthProvider from './context/AuthProvider';
+import Shipping from './component/Shipping/Shipping';
+import PrivateRoute from './component/PrivateRoute/PrivateRoute';
+import PlaceOrder from './component/PlaceOrder/PlaceOrder';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AuthProvider>
+    <BrowserRouter>
+     <Header></Header>
+     <Switch>
+       <Route exact path="/">
+      <Home></Home>
+       </Route>
+    <Route path="/home">
+    <Home></Home>
+    </Route>
+    <Route path="/login">
+    <Login></Login>
+    </Route>
+    
+    <Route path="/register"> 
+    <Register></Register>
+    </Route>
+    <PrivateRoute path="/shipping">
+    <Shipping></Shipping>
+    </PrivateRoute>
+    <PrivateRoute path="/placeorder">
+      <PlaceOrder></PlaceOrder>
+    </PrivateRoute>
+
+     </Switch>
+     
+     
+     </BrowserRouter> 
+    </AuthProvider>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBL9iqcuudsQL0k-SIX8RljDg_Sharc9hk",
+  authDomain: "simple-firebase-authenti-6938a.firebaseapp.com",
+  projectId: "simple-firebase-authenti-6938a",
+  storageBucket: "simple-firebase-authenti-6938a.appspot.com",
+  messagingSenderId: "655385506832",
+  appId: "1:655385506832:web:56072401d5370169e64695"
+};
+export default firebaseConfig;
